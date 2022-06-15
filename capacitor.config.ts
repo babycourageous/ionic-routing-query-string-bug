6a20d3c34c48f1c63ffce86af63d305fb27bb5de
Rene Dellefont <renedellefont@gmail.com>
2022-06-15 17:02:56 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-routing-query-string-bug',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
